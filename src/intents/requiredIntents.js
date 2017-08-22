5d76319b8ca41382f6a2a2e07c09a39760be208d
Brendan Bell <brendan.vadacora.bell@gmail.com>
2017-08-22 09:43:02 +0200

    Add comment for exports

--- src/intents/requiredIntents.js
+++ src/intents/requiredIntents.js
@@ -3,6 +3,7 @@ var descriptionIntent = require('./getDescriptionIntent')
 var moveIssueIntent = require('./moveIssueIntent')
 var loadIssueIntent = require('./loadIssueIntent.js')
 
+//export required intents
 module.exports = {
     'descriptionIntent': descriptionIntent,
     'moveIssueIntent': moveIssueIntent,
